782df6f60e17a57a88fa975d5cfcb0b12a72d40c
pritpatel2412 <pritptl2412@gmail.com>
2026-07-12 11:59:30 +0530

    Remove Replit dependencies

---
+++ rewrite.sh
@@ -0,0 +1,8 @@
+if [ "$GIT_COMMITTER_NAME" = "Replit Agent" ]; then
+    export GIT_COMMITTER_NAME="Kartik Rana"
+    export GIT_COMMITTER_EMAIL="ranakartik1712@gmail.com"
+fi
+if [ "$GIT_AUTHOR_NAME" = "Replit Agent" ]; then
+    export GIT_AUTHOR_NAME="Kartik Rana"
+    export GIT_AUTHOR_EMAIL="ranakartik1712@gmail.com"
+fi
