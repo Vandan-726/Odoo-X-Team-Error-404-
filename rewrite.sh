3e301761cf3417dafa0fdd96b4af387e97b2b597
pritpatel2412 <pritptl2412@gmail.com>
2026-07-12 12:07:34 +0530

    Remove rewrite.sh

--- rewrite.sh
+++
@@ -1,8 +0,0 @@
-if [ "$GIT_COMMITTER_NAME" = "Replit Agent" ]; then
-    export GIT_COMMITTER_NAME="Kartik Rana"
-    export GIT_COMMITTER_EMAIL="ranakartik1712@gmail.com"
-fi
-if [ "$GIT_AUTHOR_NAME" = "Replit Agent" ]; then
-    export GIT_AUTHOR_NAME="Kartik Rana"
-    export GIT_AUTHOR_EMAIL="ranakartik1712@gmail.com"
-fi
